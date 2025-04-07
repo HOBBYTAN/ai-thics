73e9aaad1ac4a9790cf71e302e9746cad27932b4
HOBBYTAN <pablo@hobbytan.com>
2025-04-07 15:58:38 +0900

    ESLint 오류 수정: 미사용 import 및 변수 제거

--- src/app/api/subscribers/route.ts
+++ src/app/api/subscribers/route.ts
@@ -17,8 +17,7 @@ interface SubscriberFilter {
 
 // MongoDB 클라이언트 프로미스 전역 타입 선언
 declare global {
-  // eslint-disable-next-line no-var
-  const _mongoClientPromise: Promise<MongoClient> | undefined;
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
 let client: MongoClient
